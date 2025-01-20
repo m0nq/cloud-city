acbe1f9ffe36403c20daa6944c327b483dfa1bac
Monk <forgetmenaught@gmail.com>
2025-01-20 13:14:47 -0800

    Adjusted leading and spacing of lockup content

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -7,7 +7,7 @@ export const Banner = () => {
                 <div className={styles.lockup}>
                     <h2 className={styles.h2}>Hi-Fi Vibes</h2>
                     <h1 className={styles.h1}>Cloud City</h1>
-                    <h3 className={styles.h3}>Zero-Proof Party</h3>
+                    <h3 className={styles.h3}>Zero-Proof Parties</h3>
                 </div>
             </div>
         </div>
