b317063b8450f49696d769bd62bed8a166be759c
Monk <forgetmenaught@gmail.com>
2025-01-21 17:35:38 -0800

    New mission section complete with styles

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,12 +2,14 @@ import { ReactElement } from 'react';
 
 import { Banner } from '@components/banner/banner';
 import { SignupSection } from '@components/signup/signup-section';
+import { MissionSection } from '@components/mission/mission-section';
 
 const Home = (): ReactElement => {
     return (
         <>
             <Banner />
             <SignupSection />
+            <MissionSection />
         </>
     );
 };
---
+++ src/components/mission/mission-section.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+
+import styles from './mission-section.module.css';
+import { DecorativeRings } from '@components/decorations/decorative-rings';
+
+export const MissionSection = (): ReactNode => {
+    return (
+        <div className={styles.outerContainer}>
+            <section className={styles.missionSection}>
+                <div className={styles.missionContainer}>
+                    <div className={styles.statementContainer}>
+                        <h2 className={styles.h2}>
+                            Dedicated to creating inclusive experiences that celebrate the joy of an alcohol-free lifestyle.
+                        </h2>
+                    </div>
+                    <div className={styles.decorativeRings}>
+                        <DecorativeRings rotations={[135, 154, 172]} />
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
