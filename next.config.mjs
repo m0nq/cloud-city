0ace9b340691ba8682017aba80d766c9cdf6804e
Monk <forgetmenaught@gmail.com>
2025-02-04 19:59:31 -0800

    Removed notion packages

--- next.config.mjs
+++ next.config.mjs
@@ -17,16 +17,6 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'cirquitree.com',
                 pathname: '**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'notion.so',
-                pathname: '**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
-                pathname: '**'
             }
         ]
     }
--- src/utils/api/notion-client.ts
+++
@@ -1,6 +0,0 @@
-'use server';
-import { Client } from '@notionhq/client';
-
-export const getNotionClient = async () => new Client({
-    auth: process.env.NOTION_TOKEN
-});
