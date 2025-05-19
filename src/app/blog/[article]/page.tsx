eb1f6a1b644533036391c833fe134f62e97c638a
m0nq <forgetmenaught@gmail.com>
2025-05-18 21:41:06 -0700

    Style updates for the article section

--- src/app/blog/[article]/page.tsx
+++ src/app/blog/[article]/page.tsx
@@ -9,7 +9,7 @@ import { Article } from '@components/post/article';
 import { Section } from '@components/utils/section';
 
 export const generateStaticParams = async (): Promise<{ article: string }[]> => {
-    const { posts } = await getPosts({ category: 'Blog' }, 100);
+    const { posts } = await getPosts({ category: 'Blog', tag: 'Cloud City' }, 100);
 
     return posts.map(({ post }) => ({
         // Remove leading and trailing slashes and get the last segment
